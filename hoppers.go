ab42616b5e03948f53902433b42928d5e543a71d
David Wilemski <david@davidwilemski.com>
2014-04-25 14:32:53 -0400

    Bounds checking for NewLocation()

--- hoppers.go
+++ hoppers.go
@@ -1,5 +1,7 @@
 package hoppers
 
+import "errors"
+
 const (
 	// PlayerOne string used to identify player 1
 	PlayerOne = "one"
@@ -7,6 +9,37 @@ const (
 	// PlayerTwo is a string used to identify player 2
 	PlayerTwo = "two"
 )
+
+var initialLocations = map[int32]Location{
+	1: Location{Row: 1, Col: 1},
+}
+
+// Location contains coordinates for a game board
+type Location struct {
+	Row int32
+	Col int32
+}
+
+// NewLocation instantiates a Location struct after performing bounds checks
+func NewLocation(row int32, col int32) (l Location, err error) {
+	if row <= 0 || row > 30 {
+		err = errors.New("hoppers: row value is out of bounds")
+	}
+
+	if col <= 0 || col > 30 {
+		err = errors.New("hoppers: col value is out of bounds")
+	}
+
+	l = Location{Row: row, Col: col}
+	return
+}
+
+// Piece contains state for a single piece that is on the game board
+type Piece struct {
+	Player   string
+	Num      int32
+	Location Location
+}
 
 // Board contains complete state for a game of Hoppers
 type Board struct {
--- hoppers_test.go
+++ hoppers_test.go
@@ -10,3 +10,23 @@ func TestBoardInitFirstMove(t *testing.T) {
 		t.Errorf("Board initialized with incorrect first turn: %s", board.CurrentTurn)
 	}
 }
+
+func TestNewLocation(t *testing.T) {
+	/*	var l Location*/
+	var err error
+
+	_, err = NewLocation(0, 0)
+	if err == nil {
+		t.Errorf("Locations should not allow values outside of (1, 30)")
+	}
+
+	_, err = NewLocation(1, 0)
+	if err == nil {
+		t.Errorf("Locations should not allow values outside of (1, 30)")
+	}
+
+	_, err = NewLocation(0, 1)
+	if err == nil {
+		t.Errorf("Locations should not allow values outside of (1, 30)")
+	}
+}
